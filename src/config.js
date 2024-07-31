84c0406349b12a894055b30301a51168a6a93476
duckervp <duc.trantrung99@gmail.com>
2024-07-31 18:51:38 +0700

    remove token

--- src/config.js
+++ src/config.js
@@ -6,8 +6,3 @@ export const EMAIL_REGEX = "^[a-zA-Z0-9+_.\\-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]+$";
 
 export const PAGE_SIZE = 2;
 
-export const GHN = {
-    shopId: 5231069,
-    token: "3e8c020c-4ee7-11ef-8403-eeaea5371bc8",
-    serviceId: 53321
-}
